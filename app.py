f1ea5639039caca35ae888210d184f18df92fedf
HanyongLee <suneye15@naver.com>
2018-08-30 01:41:05 +0000

    keyboard api setting

--- app.py
+++ app.py
@@ -1,19 +1,6 @@
-# 파이썬 챗봇 만들기
-
-### 카카오톡 플러스친구 관리자센터
-
-- 플러스 친구 생성 후 공개설정(공개 안되면 검색안됨 !!!)
-- 스마트 챗팅 API형 사용
-
-### C9 개발
-
-- 우측 상단의 톱니바퀴에 들어가서 python3로 설정변경
-- `sudo pip3 install flask` 플라스크 설치
-
-### keyboard
-```python3
 import os
 from flask import Flask
+#json으로 바꾸기 위해  라이브러리 추가
 import json
 
 app = Flask(__name__)
@@ -25,13 +12,15 @@ def hello():
     
 @app.route('/keyboard')
 def keyboard():
+    
+    #keyboard 딕셔너리 생성
     keyboard = {
               "type" : "buttons",
               "buttons" : ["메뉴", "로또", "고양이", "영화"]
             }
+    
+    #딕셔너리를 json으로 바꿔서 리턴 해주기 위한 코드
     json_keyboard = json.dumps(keyboard)
     return json_keyboard
     
 app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))
-
-```
